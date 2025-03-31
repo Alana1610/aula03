dda9460c9498fbdd06a1a478928e323f37f6dacd
AlanaMaria <mariaalana730@gmail.com>
2025-03-31 15:58:57 -0300

    operaçoes

---
+++ exercicio 02.py
@@ -0,0 +1,11 @@
+numero1 = float (input("escreva o primeiro numero:"))
+numero2 = float (input("escreva o segundo numero:"))
+soma = float (numero1+numero2)
+subtracao = numero1-numero2
+divisao = numero1/numero2
+multiplicaçao = numero1*numero2
+print (f"A soma dos numeros {numero1}+ {numero2} = {soma}")
+print (f"A subtracao dos numeros {numero1} - {numero2} = {subtracao} ")
+print (f"A divisao dos numeros {numero1} / {numero2} = {divisao}")
+print (f"A multiplicaçao dos numeros {numero1} * {numero2} = {multiplicaçao}")
+
