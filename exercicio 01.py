f8055bc67786b66e36398a51007b50508a1e9bdb
AlanaMaria <mariaalana730@gmail.com>
2025-03-31 14:19:27 -0300

    primeiro commit

---
+++ exercicio 01.py
@@ -0,0 +1,5 @@
+nome = input("escreva seu nome")
+idade = int (input ("escreva sua idade"))
+salario = float (input("escreva seu salario"))
+
+print(f"seu nome e {nome} sua idade e {idade} seu salario e {salario}")
