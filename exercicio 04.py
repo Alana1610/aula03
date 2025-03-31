0883e7ef1b5550f929d043e8da937580a9ca9162
AlanaMaria <mariaalana730@gmail.com>
2025-03-31 16:14:25 -0300

    else

---
+++ exercicio 04.py
@@ -0,0 +1,7 @@
+n1 = int (input("digite o numero 1 :"))
+n2 = int (input("digite o numero 2 :"))
+if  (n1 <= n2):
+    print (f"a ordem dos numeros e {n1} {n2}")
+
+else:
+    print (f"a ordem dos numeros e {n2} {n1}")
