8280eeef04b8af3158d1a69c2691b62dd20de8c2
AlanaMaria <mariaalana730@gmail.com>
2025-03-31 16:35:01 -0300

    media

---
+++ exercicio 05.py
@@ -0,0 +1,11 @@
+nota1 = float (input("escreva a primerira nota:"))
+nota2 = float (input("escreva a segunda nota:"))
+nota3 = float (input("escreva a terceira nota:"))
+media = (nota1 + nota2 + nota3) /3
+
+if media >=7:
+    print ("aprovado")
+else:
+    print ("reprovado")
+
+
